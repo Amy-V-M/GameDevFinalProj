0f6de4f061f2b87d7b0e928c43ee49204361f672
Amy-V-M <45437812+Amy-V-M@users.noreply.github.com>
2020-11-21 23:56:19 -0500

    Fixed Issues
    Had some issues trying to switch branches to get the character controller. This update should fix them.

---
+++ Assets/Scripts/QuestGiver.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestGiver : MonoBehaviour
+{
+    public Quest quest;
+
+    public GameObject questInfo;
+    public Text titleText;
+    public Text descriptionText;
+
+    private void Start()
+    {
+        questInfo.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("t"))
+        {
+            DisplayQuestWindow();
+        }
+    }
+
+    public void DisplayQuestWindow()
+    {
+        questInfo.SetActive(true);
+        titleText.text = quest.title;
+        descriptionText.text = quest.description;
+
+    }
+
+    public void AcceptQuest()
+    {
+        questInfo.SetActive(false);
+        quest.isActive = true;
+    }
+
+    public void DeclineQuest()
+    {
+        questInfo.SetActive(false);
+    }
+}
